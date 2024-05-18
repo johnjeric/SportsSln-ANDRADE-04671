6320b0c820c6d2c36c9e63243fae22a4aad5e577
John Jeric <johnjericandrade@gmail.com>
2024-05-18 16:24:24 +0800

    Initial commit

---
+++ SportsStore/SportsSln/SportsStore/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsStore.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int Totalitems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages =>
+            (int)Math.Ceiling((decimal)Totalitems / ItemsPerPage);
+    }
+}
+
